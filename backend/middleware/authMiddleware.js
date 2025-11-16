9fe61097d1e97cbb0912f70296c689cc26d8b8be
Jesus Escobar <ing.guillermo3627@gmail.com>
2025-11-15 21:41:40 -0600

    1

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,11 +3,11 @@ const User = require ("../models/usersModel")
 
 const protect = async(req, res, next) => {
 let token 
-if (req.header.authorization && req.header.authorization.startsWith("Bearer")){
+if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
 
 try {
     
-    token = req.header.authorization.split (" ")[1]
+    token = req.headers.authorization.split (" ")[1]
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
